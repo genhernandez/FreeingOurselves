a2a0f10a561e2d53adf97dca8ebe5c115db83c64
Genesis Hernandez <ghernandez@mills.edu>
2017-05-08 11:39:55 -0700

    Created html file for introduction that displayed on home page.

--- app/src/main/java/edu/mills/freeingourselves/TopFragment.java
+++ app/src/main/java/edu/mills/freeingourselves/TopFragment.java
@@ -10,6 +10,7 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
@@ -30,6 +31,7 @@ public class TopFragment extends Fragment {
     TextView noFaveWorkouts;
 //    TextView noFaveTopics;
     Cursor cursor;
+    protected WebView introWebView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,6 +43,10 @@ public class TopFragment extends Fragment {
         workoutsView = (ListView) view.findViewById(R.id.favorite_workouts_list);
 //        noFaveTopics = (TextView) view.findViewById(R.id.no_fave_topics_text);
         noFaveWorkouts = (TextView) view.findViewById(R.id.no_fave_workouts_text);
+        introWebView = (WebView)view.findViewById(R.id.introWebView);
+        introWebView.loadUrl("file:///android_asset/introduction_en.html");
+        introWebView.setVerticalScrollBarEnabled(true);
+        introWebView.setHorizontalScrollBarEnabled(true);
 
 //        // Show favorite topics
 //        new GetFaveTopicsTask().execute(view.getContext());
